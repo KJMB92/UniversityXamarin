349757c5ed45eb1f5d43a5004401218be87d3ed5
Kevin Muriel <kevinjhoel@hotmail.com>
2022-06-02 07:33:35 -0500

    Register full

--- University.App/University.App/DTOs/RegisterDTO.cs
+++ University.App/University.App/DTOs/RegisterDTO.cs
@@ -1,10 +1,26 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace University.App.DTOs
 {
-    internal class RegisterDTO
+    public class RegisterReqDTO
     {
+        [JsonProperty("email")]
+        public string Email { get; set; }
+
+        [JsonProperty("password")]
+        public string password { get; set; }
+    }
+    public class RegisterResDTO
+    {
+        [JsonProperty("token")]
+        public string Token { get; set; }
+    }
+    public class RegisterResFailDTO
+    {
+        [JsonProperty("error")]
+        public string Error { get; set; }
     }
 }
--- University.App/University.App/ViewModels/Forms/RegisterViewModel.cs
+++ University.App/University.App/ViewModels/Forms/RegisterViewModel.cs
@@ -1,10 +1,78 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.Net.Http;
 using System.Text;
+using University.App.DTOs;
+using University.App.Views.Forms;
+using Xamarin.Forms;
 
 namespace University.App.ViewModels.Forms
 {
-    internal class RegisterViewModel
+    public class RegisterViewModel : BaseViewModel
     {
+        #region Attributes
+        private string _email;
+        private string _password;
+        #endregion
+
+        #region Properties
+        public string Email
+        {
+            get { return _email; }
+            set { this.SetValue(ref _email, value); }
+        }
+
+        public string Password
+        {
+            get { return _password; }
+            set { this.SetValue(ref _password, value); }
+
+        }
+        #endregion
+
+        #region Methods
+        async void Register()
+        {
+            var data = new RegisterReqDTO { Email = this.Email, password = this.Password };
+            var json = JsonConvert.SerializeObject(data);
+            var req = new StringContent(json, Encoding.UTF8, "application/json");
+            var url = "https://reqres.in/api/register";
+            var result = string.Empty;
+
+            using (var client = new HttpClient())
+            {
+                var response = await client.PostAsync(url, req);
+
+                var statusCode = response.StatusCode;
+                result = await response.Content.ReadAsStringAsync();
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var loginRes = JsonConvert.DeserializeObject<RegisterResDTO>(result);
+                    var token = loginRes.Token;
+                    await Application.Current.MainPage.DisplayAlert("Notify Is Successful", token, "Cancel");
+                    await Application.Current.MainPage.Navigation.PushAsync(new LoginView());
+                }
+                else
+                {
+                    var registerResFail = JsonConvert.DeserializeObject<RegisterResFailDTO>(result);
+                    var error = registerResFail.Error;
+                    await Application.Current.MainPage.DisplayAlert("Notify Is Error", error, "Cancel");
+                }
+            }
+        }
+        #endregion
+
+        #region Commands
+        public Command RegisterCommand { get; set; }
+        #endregion
+
+        #region ViewsModels
+        public RegisterViewModel()
+        {
+            this.RegisterCommand = new Command(this.Register);
+        }
+        #endregion
     }
 }
